9fa39da2e2b6dbc8224147a2ca2dfd42fe9f5259
Kim Hyun <devgusdl@gmail.com>
2023-11-02 00:41:49 +0900

    최댓값

---
+++ Algorithms/BOJ/2562_최댓값/sol1.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/2562
+
+
+# numbers = []
+
+# for _ in range(9):
+#     x = int(input())
+#     numbers.append(x)
+
+numbers = [int(input()) for _ in range(9)]
+
+max_val = max(numbers)
+location = numbers.index(max_val) + 1
+
+print(max(numbers))
+print(location)
